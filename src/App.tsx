0f7e49f4d9172e67b9f5e5c6f0d9c2a391771a5c
hlysllrs <hailey.sellars@gmail.com>
2023-11-26 17:59:25 -0500

    counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { incremented, amountAdded } from './features/counter/counterSlice'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    // increment by 1
+    // dispatch(incremented())
+
+    // increment by a fixed amount
+    dispatch(amountAdded(6))
+  }
 
   return (
     <>
@@ -18,16 +28,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
