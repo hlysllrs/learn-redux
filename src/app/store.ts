5acf98af1d2b7bd99dfd5ebd883bd9240f51b449
hlysllrs <hailey.sellars@gmail.com>
2023-11-26 22:40:29 -0500

    dog api slice and query

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import { dogsApiSlice } from '../features/dogs/dogsApiSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: counterReducer,
+    [dogsApiSlice.reducerPath]: dogsApiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(dogsApiSlice.middleware)
   }
 })
 
---
+++ src/features/dogs/dogsApiSlice.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const DOG_API_KEY = import.meta.env.DOG_API_KEY
+
+interface Breed {
+  id: string
+  name: string
+  image: {
+    url: string
+  }
+}
+
+export const dogsApiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.thedogapi.com/v1',
+    prepareHeaders(headers) {
+      headers.set('x-api-key', DOG_API_KEY)
+
+      return headers
+    }
+  }),
+  endpoints(builder) {
+    return {
+      fetchBreeds: builder.query<Breed[], number | void>({
+        query(limit = 10) {
+          return `/breeds?limit=${limit}`
+        }
+      })
+    }
+  }
+})
+
+export const { useFetchBreedsQuery } = dogsApiSlice
